028a24908d89676e26646567d8ce27ae1d7c38b6
Gaurav <Gaurav@10.10.1.182>
2018-11-12 08:40:05 +0530

    added testng tag

--- src/main/java/Basepack/Base.java
+++ src/main/java/Basepack/Base.java
@@ -2,18 +2,18 @@ package Basepack;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class Base {
 	public static WebDriver driver;
 	
+	@BeforeTest
 	public static void base()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\gausing\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\work\\chromedriver_win32\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://moto.lelynx.fr/assurance-moto/comparateur-moto-vehicule.aspx");
